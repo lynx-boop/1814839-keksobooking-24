e100e075d9bcff7154da4308c991aa1af71b921d
lynx-boop <shimukurumhi@gmail.com>
2021-09-29 21:11:08 +0300

    применяет throw
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-let random = function (min, max) {
-  if (min >= max) {
-    return ('Неверный диапазон! Поменяйте числа местами или задйте другой диапазон!');
+const getRandom = (min, max) => {
+  if (min < max || min < 0) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  } else {
+    throw new getRandom;
+  }
+};
+
+console.log(getRandom(12, 20));
+
+const getFloatingRandom = (min, max, dot) => {
+  if (min > max || min < 0) {
+    return ('Неверный диапазон! Поменяйте числа местами или задайте другой диапазон!');
   }
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-
-}
-
-let randomWithDot = function (min, max, dot) {
-  if (min > max || min < 0 || max <= 0) {
-    return ('Неверный диапазон! Поменяйте числа местами или задйте другой диапазон!');
-  }
-
-  let dotsAfter = 10 ** dot;
+  const dotsAfter = 10 ** dot;
   return ((Math.random() * (max - min) + min) * dotsAfter) / dotsAfter;
-}
+};
+
+console.log(getFloatingRandom(12, 24));
+
