2173f98fa53fb9431f4d0fb7a4f5d9910e7c44d5
lynx-boop <shimukurumhi@gmail.com>
2021-11-04 00:41:11 +0300

    рефакторит код вносит правки в функции актив и дезактив
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- js/form.js
+++ js/form.js
@@ -1,94 +1,25 @@
 const adForm = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
-const mapFilter = document.querySelectorAll('.map__filter');
+const mapFilterElements = document.querySelectorAll('fieldset, select');
 
-// ПОКА НЕ СМОТРИ НА ЭТО
-// const titleInput = document.querySelector('#title');
-// const MIN_TITLE_LENGTH = 30;
-// const MAX_TITLE_LENGTH = 100;
-// const priceInput = document.querySelector('#price');
-// const MAX_PRICE = 1000000;
+const disableFormElements = (elements) => elements.forEach((element) => element.disabled = true);
+const activateFormElements = (elements) => elements.forEach((element) => element.disabled = false);
 
-// const roomNumber = document.querySelector('#room_number');
-// const capacity = document.querySelector('#capacity');
-
-// неактивное состояние
+//неактивное состояние
 const disableElements = () => {
-  const disableForm = () => {
-    adForm.classList.add('ad-form--disabled');
-    adForm.setAttribute('disabled', 'disabled');
-  };
+  adForm.classList.add('ad-form--disabled');
+  mapFilters.classList.add('.map__filters--disabled');
 
-  const disableFilter = () => {
-    mapFilters.classList.add('.map__filters--disabled');
-
-    mapFilter.forEach((filter) => {
-      filter.setAttribute('disabled', 'disabled');
-    });
-  };
-
-  disableForm();
-  disableFilter();
+  disableFormElements(mapFilterElements);
+  disableFormElements(adForm);
 };
 
 disableElements();
-
-// FIXME пыталась сделать код более универсальным, но ничего не вышло, почему?
-// const disableElement = (element) => {
-//   element.classList.add(`${element}--disabled`);
-//   element.forEach(((element) => {
-//     element.setAttribute('disabled', 'disabled');
-//   }));
-// };
-
-// disableElement(adForm);
-
 //активное состояние
-const ableElements = () => {
-  const ableForm = () => {
-    adForm.classList.remove('ad-form--disabled');
-    adForm.removeAttribute('disabled', 'disabled');
-  };
+const activateElements = () => {
+  adForm.classList.remove('ad-form--disabled');
+  mapFilters.classList.remove('.map__filters--disabled');
 
-  const ableFilter = () => {
-    mapFilters.classList.remove('.map__filters--disabled');
-
-    mapFilter.forEach((filter) => {
-      filter.removeAttribute('disabled', 'disabled');
-    });
-  };
-
-  ableForm();
-  ableFilter();
+  activateFormElements(mapFilterElements);
+  activateFormElements(adForm);
 };
-
-ableElements();
-
-// НА ЭТО ПОКА ТОЖЕ НЕ СМОТРИ, тем более оно не работает
-// валидаторы;
-// titleInput.addEventListener('input', () => {
-//   const valueLength = titleInput.value.length;
-//   if (valueLength < MIN_TITLE_LENGTH) {
-//     titleInput.setCustomValidity(`Еще ${MIN_TITLE_LENGTH - valueLength} симв.`);
-//   } else if (valueLength > MAX_TITLE_LENGTH) {
-//     titleInput.setCustomValidity(`Удалите лишние ${valueLength - MAX_TITLE_LENGTH } симв.`);
-//   } else {
-//     titleInput.setCustomValidity('');
-//   }
-
-//   titleInput.reportValidity();
-// });
-
-// priceInput.addEventListener('input', () => {
-//   if (priceInput > MAX_PRICE) {
-//     priceInput.setCustomValidity('Цена слишком большая! Максимальная цена - 1.000.000');
-//   } else {
-//     priceInput.setCustomValidity('');
-//   }
-// }); не работает! пофиксить в следующей домашке
-
-// const setRoomGuests = () => {
-//   if (roomNumber.value = 1) {
-//здесь нужно найти все option, у которых value != 1 и добавить disabled на них, но я не знаю как и не понимаю, как загуглить
-//   }
-// };
