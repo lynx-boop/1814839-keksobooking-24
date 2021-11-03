497366f5cca3c3b4e2f72aac599961e8d75505b5
lynx-boop <shimukurumhi@gmail.com>
2021-11-03 22:09:27 +0300

    пытается наметать схему выполнения домашки модуля 8
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- js/form.js
+++ js/form.js
@@ -2,6 +2,16 @@ const adForm = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
 const mapFilter = document.querySelectorAll('.map__filter');
 
+// ПОКА НЕ СМОТРИ НА ЭТО
+// const titleInput = document.querySelector('#title');
+// const MIN_TITLE_LENGTH = 30;
+// const MAX_TITLE_LENGTH = 100;
+// const priceInput = document.querySelector('#price');
+// const MAX_PRICE = 1000000;
+
+// const roomNumber = document.querySelector('#room_number');
+// const capacity = document.querySelector('#capacity');
+
 // неактивное состояние
 const disableElements = () => {
   const disableForm = () => {
@@ -22,6 +32,7 @@ const disableElements = () => {
 };
 
 disableElements();
+
 // FIXME пыталась сделать код более универсальным, но ничего не вышло, почему?
 // const disableElement = (element) => {
 //   element.classList.add(`${element}--disabled`);
@@ -33,7 +44,6 @@ disableElements();
 // disableElement(adForm);
 
 //активное состояние
-
 const ableElements = () => {
   const ableForm = () => {
     adForm.classList.remove('ad-form--disabled');
@@ -53,3 +63,32 @@ const ableElements = () => {
 };
 
 ableElements();
+
+// НА ЭТО ПОКА ТОЖЕ НЕ СМОТРИ, тем более оно не работает
+// валидаторы;
+// titleInput.addEventListener('input', () => {
+//   const valueLength = titleInput.value.length;
+//   if (valueLength < MIN_TITLE_LENGTH) {
+//     titleInput.setCustomValidity(`Еще ${MIN_TITLE_LENGTH - valueLength} симв.`);
+//   } else if (valueLength > MAX_TITLE_LENGTH) {
+//     titleInput.setCustomValidity(`Удалите лишние ${valueLength - MAX_TITLE_LENGTH } симв.`);
+//   } else {
+//     titleInput.setCustomValidity('');
+//   }
+
+//   titleInput.reportValidity();
+// });
+
+// priceInput.addEventListener('input', () => {
+//   if (priceInput > MAX_PRICE) {
+//     priceInput.setCustomValidity('Цена слишком большая! Максимальная цена - 1.000.000');
+//   } else {
+//     priceInput.setCustomValidity('');
+//   }
+// }); не работает! пофиксить в следующей домашке
+
+// const setRoomGuests = () => {
+//   if (roomNumber.value = 1) {
+//здесь нужно найти все option, у которых value != 1 и добавить disabled на них, но я не знаю как и не понимаю, как загуглить
+//   }
+// };
