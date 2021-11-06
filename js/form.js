28221a27584da04637079b15deaf225b903f5df9
lynx-boop <shimukurumhi@gmail.com>
2021-11-06 16:12:39 +0300

    убирает лишние переменные рефакторит код
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- js/form.js
+++ js/form.js
@@ -1,28 +1,32 @@
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MAX_PRICE = 1000000;
+
 const adForm = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
 const mapFilterElements = document.querySelectorAll('fieldset, select');
-
 const titleInput = document.querySelector('#title');
-const MIN_TITLE_LENGTH = 30;
-const MAX_TITLE_LENGTH = 100;
-
 const priceInput = document.querySelector('#price');
-const MAX_PRICE = 1000000;
-let minPrice = 1000;
-
 const room = document.querySelector('#room_number');
 const capacity = document.querySelector('#capacity');
 const capacityOptions = capacity.querySelectorAll('option');
 const housingType = document.querySelector('#type');
+const timeIn = document.querySelector('#timein');
+const timeOut = document.querySelector('#timeout');
+
 const roomsCapacity = {
   1: [1],
   2: [1, 2],
   3: [1, 2, 3],
   100: [0],
 };
-
-const timeIn = document.querySelector('#timein');
-const timeOut = document.querySelector('#timeout');
+const minPriceByType = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
 
 //неактивное состояние
 const disableFormElements = (elements) => elements.forEach((element) => element.disabled = true);
@@ -61,6 +65,8 @@ const onTitleInput = () => {
 };
 
 const onPriceInput = () => {
+  const minPrice = priceInput.min;
+
   if (priceInput.value > MAX_PRICE) {
     priceInput.setCustomValidity('Цена слишком большая! Максимальная цена - 1.000.000');
   } else if (priceInput.value < minPrice) {
@@ -89,19 +95,11 @@ const onRoomChange = () => {
 
 const syncCapacity = () => onRoomChange();
 
-const minPriceByType = {
-  bungalow: 0,
-  flat: 1000,
-  hotel: 3000,
-  house: 5000,
-  palace: 10000,
-};
-
 const onHousingTypeChange = () => {
   const housingValue = housingType.value;
 
-  minPrice = minPriceByType[housingValue];
-  priceInput.placeholder = minPrice;
+  priceInput.placeholder = minPriceByType[housingValue];
+  priceInput.min = minPriceByType[housingValue];
 };
 
 const syncHousingType = () => onHousingTypeChange();
@@ -110,18 +108,19 @@ const onTimeInChange = () => {
   timeOut.value = timeIn.value;
 };
 
-const onTimeOutChange = () => {
-  timeIn.value = timeOut.value;
-};
+// const onTimeOutChange = () => {
+//   timeIn.value = timeOut.value;
+// };
 
 const syncTimeIn = () => onTimeInChange();
-const syncTimeOut = () => onTimeOutChange();
+// const syncTimeOut = () => onTimeOutChange();
 
+//управляющий код
 const setFormListeners = () => {
   syncCapacity();
   syncHousingType();
   syncTimeIn();
-  syncTimeOut();
+  // syncTimeOut();
   titleInput.addEventListener('input', onTitleInput);
   priceInput.addEventListener('input', onPriceInput);
   room.addEventListener('change', onRoomChange);
