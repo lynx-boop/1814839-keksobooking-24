af94af4132e0c11dff75402e7cd597a9a0a05935
lynx-boop <shimukurumhi@gmail.com>
2021-11-08 02:53:52 +0300

    рефакторит код
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,18 +1,53 @@
 import {setFormListeners} from './form.js';
-import './map.js';
 import {createAdverts} from './data.js';
-import {createPinBaloon} from './map.js';
+import {
+  createPinBaloon,
+  createMap,
+  createPin,
+  createPinIcon
+} from './map.js';
 import {createCard} from './card.js';
 
 const SIMILAR_ADVERT_COUNT = 10;
+
+const address = document.querySelector('#address');
+
 const data = createAdverts(SIMILAR_ADVERT_COUNT);
 
-setFormListeners();
+const map = createMap(35.652832, 139.839478, 12, 'map-canvas');
+
+const mainPinIcon = createPinIcon(
+  './img/main-pin.svg',
+  [52, 52],
+  [26, 52],
+);
+
+const regularPinIcon = createPinIcon(
+  './img/pin.svg',
+  [40, 40],
+  [20, 40],
+);
 
 data.forEach((element) => {
   createPinBaloon(
     element.location.lat,
     element.location.lng,
     createCard(element),
+    regularPinIcon,
+    map,
   );
 });
+
+const mainPin = createPin(
+  35.652832,
+  139.839478,
+  true,
+  mainPinIcon,
+  map,
+);
+
+mainPin.on('moveend', (evt) => {
+  address.value = `${evt.target._latlng.lat.toFixed(5)}, ${evt.target._latlng.lng.toFixed(5)}`;
+});
+
+setFormListeners();
--- js/map.js
+++ js/map.js
@@ -20,27 +20,13 @@ const createMap = (lat, lng, scale, selector) => {
   return mapCanvas;
 };
 
-const map = createMap(35.652832, 139.839478, 12, 'map-canvas');
-
 const createPinIcon = (url, size, anchor) => L.icon({
   iconUrl: url,
   iconSize: size,
   iconAnchor: anchor,
 });
 
-const mainPinIcon = createPinIcon(
-  './img/main-pin.svg',
-  [52, 52],
-  [26, 52],
-);
-
-const regularIcon = createPinIcon(
-  './img/pin.svg',
-  [40, 40],
-  [20, 40],
-);
-
-const createPin = (lat, lng, drg, icn) => {
+const createPin = (lat, lng, drg, icn, map) => {
   const pin = L.marker(
     {
       lat: lat,
@@ -55,23 +41,14 @@ const createPin = (lat, lng, drg, icn) => {
   return pin;
 };
 
-const mainPin = createPin(
-  35.652832,
-  139.839478,
-  true,
-  mainPinIcon);
-
-const address = document.querySelector('#address');
-
-mainPin.on('moveend', (evt) => {
-  address.value = `${evt.target._latlng.lat.toFixed(5)}, ${evt.target._latlng.lng.toFixed(5)}`;
-});
-
-//
-
-const createPinBaloon = (lat, lng, baloonCntnt) => {
-  const p = createPin(lat, lng, false, regularIcon);
+const createPinBaloon = (lat, lng, baloonCntnt, icn, map) => {
+  const p = createPin(lat, lng, false, icn, map);
   p.bindPopup(baloonCntnt);
 };
 
-export {createPinBaloon};
+export {
+  createPinBaloon,
+  createMap,
+  createPin,
+  createPinIcon
+};
