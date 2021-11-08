1e3da721fad7f00b1012779bc0340200ef619688
lynx-boop <shimukurumhi@gmail.com>
2021-11-09 00:38:07 +0300

    вносит часть правок
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- js/map.js
+++ js/map.js
@@ -1,24 +1,38 @@
 import {activateElements} from './form.js';
+import {createAdverts} from './data.js';
+import {createCard} from './card.js';
 
-const createMap = (lat, lng, mapZoom, selector) => {
-  const mapCanvas = L.map(selector)
-    .on('load', () => {
-      activateElements();
-    })
+const address = document.querySelector('#address');
+const tileLayer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tileLayerAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-    .setView({
-      lat: lat,
-      lng: lng,
-    }, mapZoom);
+const SIMILAR_ADVERT_COUNT = 10;
+const MAIN_PIN_SIZE = 52;
+const REGULAR_PIN_SIZE = 40;
 
-  L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    },
-  ).addTo(mapCanvas);
-  return mapCanvas;
+const tokyo = {
+  lat: 35.658581,
+  lng: 139.745438,
+  mapZoom: 12,
 };
+
+const map = L.map('map-canvas')
+  .on('load', () => {
+    activateElements();
+  })
+
+  .setView({
+    lat: tokyo.lat,
+    lng: tokyo.lng,
+  }, tokyo.mapZoom);
+
+L.tileLayer(
+  tileLayer,
+  {
+    attribution: tileLayerAttribution,
+  },
+).addTo(map);
+
 
 const createPinIcon = (url, size, anchor) => L.icon({
   iconUrl: url,
@@ -38,7 +52,7 @@ const createPin = (lat, lng, draggable, icon, cityMap) => {
     },
   );
   pin.addTo(cityMap);
-  return pin;
+  return pin;// не надо ретурнами, надо как в образце
 };
 
 const createPinBaloon = (lat, lng, baloonCntnt, icon, cityMap) => {
@@ -46,9 +60,62 @@ const createPinBaloon = (lat, lng, baloonCntnt, icon, cityMap) => {
   pin.bindPopup(baloonCntnt);
 };
 
+const mainPinIcon = createPinIcon(
+  './img/main-pin.svg',
+  [MAIN_PIN_SIZE, MAIN_PIN_SIZE],
+  [MAIN_PIN_SIZE / 2 , MAIN_PIN_SIZE],
+);
+
+const regularPinIcon = createPinIcon(
+  './img/pin.svg',
+  [REGULAR_PIN_SIZE, REGULAR_PIN_SIZE],
+  [REGULAR_PIN_SIZE / 2, REGULAR_PIN_SIZE],
+);
+
+const mainPin = createPin(
+  tokyo.lat,
+  tokyo.lng,
+  true,
+  mainPinIcon,
+  map,
+);
+
+mainPin.on('moveend', (evt) => {
+  address.value = `${evt.target._latlng.lat.toFixed(5)}, ${evt.target._latlng.lng.toFixed(5)}`;
+});
+
+const data = createAdverts(SIMILAR_ADVERT_COUNT);
+
+data.forEach((element) => {
+  createPinBaloon(
+    element.location.lat,
+    element.location.lng,
+    createCard(element),
+    regularPinIcon,
+    map,
+  );
+});
+
+address.value = `${tokyo.lat.toFixed(5)}, ${tokyo.lng.toFixed(5)}`;
+
+const initMap = () => {
+  map.on('whenReady', () => {
+    activateElements();
+  })
+
+    .setView({
+      lat: tokyo.lat,
+      lng: tokyo.lng,
+    }, tokyo.mapZoom);
+
+  L.tileLayer(
+    tileLayer,
+    {
+      attribution: tileLayerAttribution,
+    },
+  ).addTo(map);
+};
+
 export {
-  createPinBaloon,
-  createMap,
-  createPin,
-  createPinIcon
+  initMap
 };
